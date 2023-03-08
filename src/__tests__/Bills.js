685dfa78dac62ccd39b6a257bc85e041fce2cb92
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2023-03-08 02:35:03 +0100

    fixer mon test de Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -17,29 +17,37 @@ import userEvent from "@testing-library/user-event";
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      Object.defineProperty(window, 'localStorage', {
+         value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        'user', 
+        JSON.stringify({
+          type: 'Employee',
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId('icon-window'));
+      const windowIcon = screen.getByTestId('icon-window');
       //to-do write expect expression
       expect(windowIcon.className).toContain("active-icon");
-    })
+    });
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : 0)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+          )
+        .map(a => a.innerHTML);
+      const antiChrono = (a, b) => ((a < b) ? 1 : 0);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
 
   describe("When I click on 'Nouvelle note de frais' button", () => {
     test("Then it should redirect to NewBill page", () => {
@@ -53,13 +61,13 @@ describe("Given I am connected as an employee", () => {
 
       const onNavigate = (pathname) => {
         document.body.innerHTML = pathname;
-      }
+      };
 
       const billsContainer = new Bills({
         document,
         onNavigate,
         store: mockStore,
-        localStorage: window.localStorage
+        localStorage: window.localStorage,
       });
 
       const button = screen.getByTestId("btn-new-bill");
@@ -67,8 +75,8 @@ describe("Given I am connected as an employee", () => {
       button.addEventListener("click", handleClickNewBill);
       userEvent.click(button);
       expect(handleClickNewBill).toHaveBeenCalled();
-    })
-  })
+    });
+  });
 
   describe("When I click on the 'IconEye' button", () => {
     test("Then a modal should open", () => {
@@ -84,7 +92,7 @@ describe("Given I am connected as an employee", () => {
         document,
         onNavigate,
         store: mockStore,
-        localStorage: window.localStorage
+        localStorage: window.localStorage,
       });
 
       $.fn.modal = jest.fn();
@@ -98,12 +106,15 @@ describe("Given I am connected as an employee", () => {
 
       expect(handleShowModalFile).toHaveBeenCalled();
       expect(screen.getAllByText("Justificatif")).toBeTruthy();
-    })
-  })
+    });
+  });
 
   describe("Fetch data", () => {
     test("Then fetch data", async () => {
-      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "joanna.binet@billed.com" }));
+      localStorage.setItem(
+        "user", 
+        JSON.stringify({ type: "Employee", email: "joanna.binet@billed.com" })
+      );
 
       const element = document.createElement("div");
       element.setAttribute("id", "root");
@@ -113,7 +124,7 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByText("Mes notes de frais"));
       expect(screen.getByText("Mes notes de frais")).toBeTruthy();
-    })
+    });
 
     describe("When I fetch bills", () => {
       test("Then it should display bills", async () => {
@@ -121,15 +132,15 @@ describe("Given I am connected as an employee", () => {
           document,
           onNavigate,
           store: mockStore,
-          localStorage: window.localStorage
+          localStorage: window.localStorage,
         });
 
         const getBills = jest.fn(() => bills.getBills());
         const result = await getBills();
         expect(getBills).toHaveBeenCalled();
         expect(result.length).toBe(4);
-      })
-    })
+      });
+    });
 
     test("Then it should return error 404 message", async () => {
       mockStore.bills = jest.fn().mockImplementation(() => {
@@ -139,7 +150,7 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = html;
       const errorMessage = screen.getByText(/Erreur 404/);
       expect(errorMessage).toBeTruthy();
-    })
+    });
 
     test("Then it should return error 500 message", async () => {
       mockStore.bills = jest.fn().mockImplementation(() => {
@@ -149,6 +160,6 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = html;
       const errorMessage = screen.getByText(/Erreur 500/);
       expect(errorMessage).toBeTruthy
-    })
-  })
-})
+    });
+  });
+});
